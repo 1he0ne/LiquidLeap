5be794f657f3ae85ef344baf83eb02adf779c456
Eduard Pranz <1703839+1he0ne@users.noreply.github.com>
2024-05-13 01:27:27 +0200

    added gun rotation

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -8,9 +8,33 @@ public class PlayerShoot : MonoBehaviour
 
     public Transform AimingPoint;
 
+    public GameObject Gun;
+    public SpriteRenderer GunSprite;
+
+    public Vector3 WorldMousePos;
+    public Vector2 Direction;
+
     void Update()
     {
-        if( Input.GetMouseButton(0) )
+        WorldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Direction = (Vector2)(WorldMousePos - transform.position).normalized;
+
+        float angle = Mathf.Atan2(Direction.y, Direction.x) * Mathf.Rad2Deg;
+        Quaternion targetRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        Vector3 targetPosition = AimingPoint.position + (Vector3)(Direction * 0.5f);
+
+        Gun.transform.position = targetPosition;
+
+        Gun.transform.rotation = targetRotation;
+
+        // TODO: flip gun, when rotated accordingly
+        if (Mathf.Abs(angle) > 90)
+        {
+            GunSprite.flipX = true;  
+        }
+
+        if ( Input.GetMouseButton(0) )
         {
             Shoot();
         }
@@ -18,14 +42,11 @@ public class PlayerShoot : MonoBehaviour
 
     void Shoot()
     {
-        Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        Vector2 direction = (Vector2)((worldMousePos - transform.position)); direction.Normalize();
 
         // Creates the bullet locally
-        GameObject bullet = Instantiate(WaterPrefab,AimingPoint.position + (Vector3)(direction * 0.5f),Quaternion.identity);
+        GameObject bullet = Instantiate(WaterPrefab,AimingPoint.position + (Vector3)(Direction * 0.5f),Quaternion.identity);  
 
         // Adds velocity to the bullet
-        bullet.GetComponent<Rigidbody2D>().velocity = direction * Force;
+        bullet.GetComponent<Rigidbody2D>().velocity = Direction * Force;
     }
 }

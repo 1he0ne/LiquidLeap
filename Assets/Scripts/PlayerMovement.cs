51a9dca6aee80452490458ddf7fa202a599ca54b
Eduard Pranz <1703839+1he0ne@users.noreply.github.com>
2024-05-15 19:20:31 +0200

    added jump, land, walk sounds

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -19,10 +19,19 @@ public class PlayerMovement : MonoBehaviour
     private static int JumpCooldown = 0;
 
     private float MoveDir;
+
+    private AudioSource JumpSound;
+    private AudioSource WalkSound;
+    private AudioSource LandFloorSound;
     void Start()
     {
         Rb = GetComponent<Rigidbody2D>();
         SpriteRenderer = GetComponent<SpriteRenderer>();
+
+        var AudioSource = GetComponents<AudioSource>();
+        JumpSound = AudioSource[0];
+        WalkSound = AudioSource[1];
+        LandFloorSound = AudioSource[2];
     }
 
     // Update is called once per frame
@@ -30,34 +39,50 @@ public class PlayerMovement : MonoBehaviour
     {
         Moving = false;
 
-        if ( Input.GetKey(KeyCode.D) )
+        if (Input.GetKey(KeyCode.D))
         {
             MoveDir = Input.GetAxis("Horizontal");
             Rb.velocity = new Vector2(MoveDir * MovementSpeed, Rb.velocity.y);
             Moving = true;
             SpriteRenderer.flipX = false;
         }
-        
-        if ( Input.GetKey(KeyCode.A) )
+
+        if (Input.GetKey(KeyCode.A))
         {
             MoveDir = Input.GetAxis("Horizontal");
             Rb.velocity = new Vector2(MoveDir * MovementSpeed, Rb.velocity.y);
             Moving = true;
             SpriteRenderer.flipX = true;
-           
+
         }
 
+        bool wasGrounded = isGrounded;
+
         isGrounded = Physics2D.Raycast(GroundCheck1.position, -transform.up, 0.1f, GroundLayers) || Physics2D.Raycast(GroundCheck2.position, -transform.up, 0.1f, GroundLayers);
+        if (!wasGrounded && isGrounded)
+        {
+            LandFloorSound.Play();
+        }
 
         if (Input.GetKey(KeyCode.Space) && isGrounded && JumpCooldown == 0)
         {
             JumpCooldown += 3;
             Rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
             Moving = false;
+            JumpSound.Play();
         }
         else if (isGrounded)
         {
             JumpCooldown = Mathf.Max(JumpCooldown - 1, 0);
         }
-    }  
+
+        if (!Moving)
+        {
+            WalkSound.Stop();
+        }
+        else if (!WalkSound.isPlaying && isGrounded)
+        {
+            WalkSound.Play();
+        }
+    }
 }

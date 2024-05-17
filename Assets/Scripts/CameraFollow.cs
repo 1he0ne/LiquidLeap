d3509992a637fda5357af45c4db20654cb9604bb
Eduard Pranz <1703839+1he0ne@users.noreply.github.com>
2024-05-17 19:36:41 +0200

    added scenes to be loaded in build settings. added string input for game end script

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform PlayerPosition;
     public Vector3 Offset;
 
     private Camera Camera;
+    private Transform PlayerPosition;
 
     private void Start()
     {
+        PlayerPosition = GameObject.FindGameObjectsWithTag("Player")[0].transform;
         Camera = GetComponent<Camera>();
     }
     void Update()
